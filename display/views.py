0f8eae95cd10b06fefc5e0b118d1e8b0e5c8dad4
Omsurvase24 <omsurvase24@gmail.com>
2024-02-19 04:19:07 +0530

    table

--- display/views.py
+++ display/views.py
@@ -7,3 +7,7 @@ def home(request):
     response = requests.get(
         'https://mpulse-ignite-backend.up.railway.app/api/register').json()
     return render(request, 'home.html', {'response': response})
+
+
+def info(request):
+    return render(request, 'info.html')
--- fetchapi/urls.py
+++ fetchapi/urls.py
@@ -17,8 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
-from django.conf.urls.static import static
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+# from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
