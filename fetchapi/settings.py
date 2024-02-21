ef73355dc36c019839fa73ec9b91feaf2f054a0e
Omsurvase24 <omsurvase24@gmail.com>
2024-02-21 12:45:36 +0530

    deploy

--- fetchapi/settings.py
+++ fetchapi/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-j&_yge+lrte82a57)=!zndag^mw7r4b*$jx99ppit)@=oxeu67'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
